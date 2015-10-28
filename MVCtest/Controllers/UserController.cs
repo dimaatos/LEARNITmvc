e6d86f8ddf90c48d57da998b41408c26d981106e
VeraKH <vera_khatskevich@sibmail.com>
2015-10-28 23:06:52 +0300

    model

--- MVCtest/Controllers/UserController.cs
+++ MVCtest/Controllers/UserController.cs
@@ -8,10 +8,38 @@ namespace MVCtest.Controllers
 {
     public class UserController : Controller
     {
-        // GET: User
-        public ActionResult UserPage()
+       UsersEntities db = new UsersEntities();
+
+
+        public ActionResult Register()
         {
-            return View();
+            var NewUser = new UserTable();
+            return View(NewUser);
+        }
+
+
+
+        public ActionResult Register(UserTable model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                UserTable user = new UserTable();
+
+                user.loginName = model.loginName;
+                user.userEmail = model.userEmail;
+                user.userPassword = model.userPassword;
+
+                db.UserTables.Add(model);
+
+                return View(model);
+            }
+
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
         }
     }
 }
