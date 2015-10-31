c1a5057dba42c031837e42496c5f011b6c9f40b2
VeraKH <vera_khatskevich@sibmail.com>
2015-10-31 10:34:51 +0300

    adding fields to  IdentityModel AspNet users table

--- MVCtest/Models/IdentityModels.cs
+++ MVCtest/Models/IdentityModels.cs
@@ -4,12 +4,36 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace MVCtest.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public ApplicationUser()
+        {
+            JoindDay = DateTime.Now;
+        }
+
+        [Display(Name = "Joined us on")]
+        public DateTime JoindDay { get; set; }
+
+        [Display(Name = "Username")]
+        public string Name { get; set; }
+
+        [Display(Name = "Your photo")]
+        public string ProfilePicUrl { get; set; }
+
+        [Display(Name = "Your location")]
+        public string Location { get; set; }
+
+        [Display(Name = "Tell about yourself")]
+        public string About { get; set; }
+
+        [Display(Name = "Your courses")]
+        public virtual ICollection<CourseModel> Courses { get; set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
