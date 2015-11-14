c9314b20f718d6c1a7979964c247ee2fa9bc3f4d
VeraKH <vera_khatskevich@sibmail.com>
2015-11-14 17:26:43 +0300

    adding areas

--- LEARNIT/LEARNIT/App_Start/RouteConfig.cs
+++ LEARNIT/LEARNIT/App_Start/RouteConfig.cs
@@ -16,7 +16,9 @@ namespace LEARNIT
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] {"Areas.Controllers"}
+                
             );
         }
     }
